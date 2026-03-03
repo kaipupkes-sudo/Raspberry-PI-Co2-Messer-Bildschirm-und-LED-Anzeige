30ad6a5c760d050c8668f848803fd6ccd677b406
Yannick Proegler <v3rpro3gl3r@googlemail.com>
2026-03-03 12:00:19 +0100

    ungetestet

--- InitialBlink/ButtonBlink.py
+++ InitialBlink/ButtonBlink.py
@@ -9,9 +9,17 @@ output_GPIO = 22
 GPIO.setup(input_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(output_GPIO, GPIO.OUT)
 
-while True:
-	if GPIO.input(input_GPIO):
-		print("BUTTON PRESSED")
-		GPIO.OUTPUT(output_GPIO, GPIO.HIGH)
-		time.sleep(3)
-		GPIO.OUTPUT(output_GPIO, GPIO.LOW)
+try:
+    while True:
+        if GPIO.input(input_GPIO):
+            print("BUTTON PRESSED")
+            GPIO.output(output_GPIO, GPIO.HIGH)
+            time.sleep(3)
+            GPIO.output(output_GPIO, GPIO.LOW)
+        time.sleep(0.01)
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    GPIO.cleanup()
