5195d108a6e4f0ca4601bbe195eb189b110c0ef4
Yannick Proegler <v3rpro3gl3r@googlemail.com>
2026-03-03 11:40:55 +0100

    ungetestet

---
+++ InitialBlink/ButtonBlink.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+input_GPIO = 21
+output_GPIO = 22
+
+GPIO.setup(input_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(output_GPIO, GPIO.OUT)
+
+while True:
+	if GPIO.input(input_GPIO):
+		print("BUTTON PRESSED")
+		GPIO.OUTPUT(output_GPIO, GPIO.HIGH)
+		time.sleep(3)
+		GPIO.OUTPUT(output_GPIO, GPIO.LOW)
